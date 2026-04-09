2c7569481683dc6e4ca4485fe7ca8655563575ff
denyse-tan <denyse@koobits.com>
2026-04-09 11:12:01 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // ADD THIS LINE BELOW (The dot-slash ensures it works on any path)
-    base: './', 
+    // CHANGE THIS LINE TO MATCH YOUR REPO NAME EXACTLY
+    base: '/KBCCS2_RWM2026_Template/', 
+    
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
